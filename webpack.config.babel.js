754be46584ca68c04db7952ad1d1bc94ba68e0d9
nokl <naoki.tkb@gmail.com>
2019-10-21 19:42:17 +0900

    update: core-js config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -31,7 +31,15 @@ export const webpackConfig = {
                     {
                         loader: 'babel-loader',
                         options: {
-                            presets: ['@babel/preset-env'],
+                            presets: [
+                                [
+                                    '@babel/preset-env',
+                                    {
+                                        useBuiltIns: 'usage',
+                                        corejs: 3,
+                                    },
+                                ],
+                            ],
                         },
                     },
                 ],
